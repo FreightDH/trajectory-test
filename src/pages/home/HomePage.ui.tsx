df1f70a1c8572d39f9f9c7bcdf42a0b8bc803e11
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-07-12 16:51:35 +0300

    feat: add edit functionality

--- src/pages/home/HomePage.ui.tsx
+++ src/pages/home/HomePage.ui.tsx
@@ -1,4 +1,5 @@
 import { type FC, type ReactElement } from 'react';
+import { Select } from 'antd';
 
 import { useFetch } from '@/shared/lib';
 import { Car } from '@/shared/UI/Car';
@@ -6,7 +7,20 @@ import { Car } from '@/shared/UI/Car';
 import cl from './HomePage.module.scss';
 
 export const HomePage: FC = (): ReactElement => {
-  const [cars, , isLoading, isError] = useFetch('https://test.tspb.su/test-task/vehicles');
+  const [cars, setCars, isLoading, isError] = useFetch('https://test.tspb.su/test-task/vehicles');
+
+  const handleSort = (value: string) => {
+    switch (value) {
+      case 'year':
+        setCars([...cars!].sort((a, b) => a.year - b.year));
+        break;
+      case 'price':
+        setCars([...cars!].sort((a, b) => a.price - b.price));
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <main className={cl.page}>
@@ -16,7 +30,24 @@ export const HomePage: FC = (): ReactElement => {
             {isLoading && <h1 className={cl.cars__title}>Loading...</h1>}
             {isError && <h1 className={cl.cars__title}>Something went wrong! Try again later.</h1>}
             <h1 className={cl.cars__title}>Cars</h1>
-            <ul className={cl.cars__list}>{cars && cars.map((car) => <Car key={car.id} car={car} />)}</ul>
+            <Select
+              placeholder="Sort by"
+              style={{ width: '20%', alignSelf: 'end' }}
+              options={[
+                {
+                  label: 'Year',
+                  value: 'year',
+                },
+                {
+                  label: 'Price',
+                  value: 'price',
+                },
+              ]}
+              onChange={handleSort}
+            />
+            <ul className={cl.cars__list}>
+              {cars && cars.map((car) => <Car key={car.id} car={car} setCars={setCars} />)}
+            </ul>
           </div>
         </div>
       </section>
